73c2dffb3858682d28f052c03f50d7da2b1cdfac
Shuvy <shuvyan@gmail.com>
2018-06-20 16:51:06 +0300

    add editor

---
+++ js/service-editor.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gMeme = {
+    selectedImgId: 5,
+    txts: [
+        {
+            line: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+};
