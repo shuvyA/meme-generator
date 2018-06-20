7408052649c94bae2093b23f3c42ce68d2ab7fff
markop85 <mark.kopolov@gmail.com>
2018-06-20 18:40:25 +0300

    service-gallery

--- js/main-gallery.js
+++ js/main-gallery.js
@@ -1,20 +1,22 @@
 'use strict'
 
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['happy']},
-{id:2, url:'img/2.jpg', keywords: ['sad']},
-{id:3, url:'img/3.jpg', keywords: ['happy']},
-{id:4, url:'img/4.jpg', keywords: ['happy']},
-{id:5, url:'img/5.jpg', keywords: ['happy']}
-];
+
 renderPhotos();
 
 function renderPhotos() {
     var strHTMLS = '';
     gImgs.forEach(function(img, idx){
-        strHTMLS += `<li class = "photoContainer flex">
+        strHTMLS += `<li class = "photoContainer flex" onclick = "openEditor(${img.id})">
         <img src="${img.url}">
         </li>`
     });
     var imgContainer = document.querySelector('.the-gallery');
     imgContainer.innerHTML = strHTMLS;
+}
+
+function openEditor(imgId) {
+    gMeme.selectedImgId = imgId;
+    window.open("editor.html");
+    console.log(gMeme);
+    
 }
