db3eea4ce6dd643795413a1568c82aebad64e2f2
Shuvy <shuvyan@gmail.com>
2018-06-20 17:34:33 +0300

    shuvy

--- js/main-editor.js
+++ js/main-editor.js
@@ -17,22 +17,31 @@ function initEditor() {
 
 
 function renderImg() {
+
+    gMeme.txts[0].url = "/img/2.jpg";
+
+    var imgUrl = gMeme.txts[0].url;
+
     var ctx = canvas.getContext("2d");
     var img = new Image();
     img.onload = function () {
         ctx.drawImage(img, 0, 0, 250, 250);
     }
-    img.src = "/img/2.jpg";
+    img.src = imgUrl;
 }
 
 
 
 function GetTxtFromUser() {
 
-
+    return document.querySelector('.txt-user').value;
 }
 
 
+function renderReset() {
+    renderImg()
+
+}
 
 function renderTxt() {
 
@@ -40,17 +49,19 @@ function renderTxt() {
     var fontSize = gMeme.txts[0].size;
     var color = gMeme.txts[0].color;
 
-    txtFromUser = document.querySelector('.txt-user').value;
+    txtFromUser = GetTxtFromUser();
+    // txtFromUser = document.querySelector('.txt-user').value;
 
     // console.log(txtFromUser);
+    canvas.width = 300;
+    canvas.height = 300;
+    
     var ctx = canvas.getContext("2d");
     // ctx.clearRect(10, 50, 0, 0);
 
     ctx.font = "30px Arial";
     ctx.fillStyle = color;
     ctx.fillText(txtFromUser, 10, 50);
-
-    // ctx.fillText(gMeme.txts[0].line, 10, 50);
 }
 
 
