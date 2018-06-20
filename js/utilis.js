a143361d64aed288dd1b4b5f4368c2920c58d0f9
Shuvy <shuvyan@gmail.com>
2018-06-20 14:42:04 +0300

    utilis

---
+++ js/utilis.js
@@ -0,0 +1,19 @@
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+function loadFromStorage(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+
+function makeId() {
+    var length = 6;
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return txt;
+}
