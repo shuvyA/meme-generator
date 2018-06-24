278fa33c5fecd0fd135e1c9a055ef965bd82ad1b
Shuvy <shuvyan@gmail.com>
2018-06-24 19:56:26 +0300

    so

--- js/utilis.js
+++ js/utilis.js
@@ -14,18 +14,14 @@ function makeId() {
     for (var i = 0; i < length; i++) {
         txt += possible.charAt(Math.floor(Math.random() * possible.length));
     }
-
     return txt;
 }
 
 
 function openNavMobile(x) {
-    // debugger;
     var change = document.querySelector('.nav-mobile')
     change.classList.toggle("change");
-    // x.classList.toggle("change");
     var menu = document.querySelector('ul.main-menu');
-    // console.log('main menu', menu)
     menu.classList.toggle('open');
     var opac = document.querySelector('.toggle-menu-screen')
     opac.classList.toggle('show-div');
@@ -35,7 +31,6 @@ function openNavMobile(x) {
 function closeNavMobile() {
     var change = document.querySelector('.nav-mobile')
     change.classList.remove("change");
-
     
     var menu = document.querySelector('ul.main-menu');
     menu.classList.remove('open');
